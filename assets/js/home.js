634cf265521e10a5dbafaf3206816976c00ec1e9
Breno Brito <breno.silvabrito@hotmail.com>
2023-03-10 12:06:51 +0000

    Add Forkify project to the portfolio

---
+++ assets/js/home.js
@@ -0,0 +1,20 @@
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+const nav = document.querySelector(".navbar");
+
+const observer = new IntersectionObserver(
+  function (entry) {
+    if (entry.isIntersecting) {
+      navbar.style.display = "none"; // Hide the navbar when the header is in the viewport
+    } else {
+      navbar.style.display = "flex"; // Show the navbar when the header is out of the viewport
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-200px",
+  }
+);
+
+observer.observe(header);
