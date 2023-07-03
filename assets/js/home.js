32d5120dab1f8f95e33d587edd1f2bd172f95c37
Breno Brito <breno.silvabrito@hotmail.com>
2023-07-03 14:01:30 +0100

    Remove navbar from contact page

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,20 +1,21 @@
-const body = document.querySelector("body");
-const header = document.querySelector("header");
-const nav = document.querySelector(".navbar");
+// const body = document.querySelector("body");
+// const header = document.querySelector("header");
+// const nav = document.querySelector(".navbar");
 
-const observer = new IntersectionObserver(
-  function (entry) {
-    if (entry.isIntersecting) {
-      navbar.style.display = "none"; // Hide the navbar when the header is in the viewport
-    } else {
-      navbar.style.display = "flex"; // Show the navbar when the header is out of the viewport
-    }
-  },
-  {
-    root: null,
-    threshold: 0,
-    rootMargin: "-200px",
-  }
-);
+// Not being used
+// const observer = new IntersectionObserver(
+//   function (entry) {
+//     if (entry.isIntersecting) {
+//       navbar.style.display = "none"; // Hide the navbar when the header is in the viewport
+//     } else {
+//       navbar.style.display = "flex"; // Show the navbar when the header is out of the viewport
+//     }
+//   },
+//   {
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-200px",
+//   }
+// );
 
-observer.observe(header);
+// observer.observe(header);
